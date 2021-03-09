2dda86bd0feed37274351df8dcc245ee5771cfde
wlodarsb <wlodarsb@coventry.ac.uk>
2021-03-09 17:31:21 +0000

    Added logic for horizontal displays

--- app/src/main/java/bwlodarski/courseworkapplication/Activities/PhotoViewActivity.java
+++ app/src/main/java/bwlodarski/courseworkapplication/Activities/PhotoViewActivity.java
@@ -1,66 +1,44 @@
 package bwlodarski.courseworkapplication.Activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.widget.GridView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-import java.util.ArrayList;
-
-import bwlodarski.courseworkapplication.Adapters.PhotoAdapter;
-import bwlodarski.courseworkapplication.Models.Photo;
+import bwlodarski.courseworkapplication.Fragments.PhotoGridFragment;
+import bwlodarski.courseworkapplication.Fragments.PhotoMultiViewFragment;
 import bwlodarski.courseworkapplication.R;
 
 public class PhotoViewActivity extends AppCompatActivity {
-
-
-	GridView photoGrid;
-	ArrayList<Photo> photos = new ArrayList<>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photo_view);
 		Toolbar toolbar = findViewById(R.id.toolbar);
+		toolbar.setTitle("Your photos");
 		setSupportActionBar(toolbar);
+
+		int orientation = getResources().getConfiguration().orientation;
+
+		if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+			// Replacing `photo_view` with the photo grid fragment
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.photo_view, new PhotoGridFragment()).commit();
+		} else {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.photo_view, new PhotoMultiViewFragment()).commit();
+		}
 
 		FloatingActionButton fab = findViewById(R.id.addPhoto);
 		fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 				.setAction("Action", null).show());
 
-		photoGrid = findViewById(R.id.photo_grid);
-		photos.add(new Photo(R.drawable.img_1));
-		photos.add(new Photo(R.drawable.img_2));
-		photos.add(new Photo(R.drawable.img_3));
-		photos.add(new Photo(R.drawable.img_4));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-		photos.add(new Photo(R.drawable.img_5));
-
-
-		PhotoAdapter photoAdapter = new PhotoAdapter(this, R.layout.grid_photo, photos);
-		photoGrid.setAdapter(photoAdapter);
 	}
+
+
 }

33f0078315b02ff5f55ba888458eec26fae8216b
wlodarsb <wlodarsb@coventry.ac.uk>
2021-04-06 22:28:58 +0100

    Updated Photo class to match requirements

--- app/src/main/java/bwlodarski/courseworkapplication/Models/Photo.java
+++ app/src/main/java/bwlodarski/courseworkapplication/Models/Photo.java
@@ -1,9 +1,27 @@
 package bwlodarski.courseworkapplication.Models;
 
 public class Photo {
-	int image;
+	int id;
+	String image;
 
-	public Photo(int image) { this.image = image; }
+	public Photo(int id, String image) {
+		this.id = id;
+		this.image = image;
+	}
 
-	public int getImage() { return image; }
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
 }

eab4f9f8e427425e4f477e28c6071f4ff1173cc5
wlodarsb <wlodarsb@coventry.ac.uk>
2021-04-06 22:32:44 +0100

    Updated details view to use Strings instead of ints for image data
    We are no longer using resources, now bitmaps are used.

--- app/src/main/java/bwlodarski/courseworkapplication/Activities/PhotoDetailsActivity.java
+++ app/src/main/java/bwlodarski/courseworkapplication/Activities/PhotoDetailsActivity.java
@@ -1,6 +1,7 @@
 package bwlodarski.courseworkapplication.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -8,6 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import bwlodarski.courseworkapplication.Helpers.ImageHandler;
 import bwlodarski.courseworkapplication.R;
 
 public class PhotoDetailsActivity extends FragmentActivity {
@@ -18,9 +20,11 @@ public class PhotoDetailsActivity extends FragmentActivity {
 		setContentView(R.layout.activity_photo_details);
 
 		Intent intent = getIntent();
-		int id = intent.getIntExtra("photo", 0);
+		String image = intent.getStringExtra("photo");
+
+		Bitmap imgBitmap = ImageHandler.getBitmap(image);
 
 		ImageView bigPhoto = findViewById(R.id.big_photo);
-		bigPhoto.setImageResource(id);
+		bigPhoto.setImageBitmap(imgBitmap);
 	}
 }

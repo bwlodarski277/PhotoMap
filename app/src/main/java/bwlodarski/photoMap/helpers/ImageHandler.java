5326325bac3778b55f99d3cc2a4dcf04eaf830a3
wlodarsb <wlodarsb@coventry.ac.uk>
2021-04-23 01:07:43 +0100

    Docstrings

--- app/src/main/java/bwlodarski/photoMap/helpers/ImageHandler.java
+++ app/src/main/java/bwlodarski/photoMap/helpers/ImageHandler.java
@@ -2,7 +2,6 @@ package bwlodarski.photoMap.helpers;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Base64;
 
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -17,33 +16,14 @@ import java.io.IOException;
  * Can convert images to bitmaps and base-64 encoded strings.
  */
 public final class ImageHandler {
-	/**
-	 * Generates a Bitmap from an image string from the database.
-	 *
-	 * @param imageString image string from the database
-	 * @return Bitmap created from the image
-	 */
-	public static Bitmap stringToBitmap(String imageString) {
-		byte[] bytes = Base64.decode(imageString, Base64.URL_SAFE);
-		return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-	}
 
 	/**
-	 * Generates database-safe string from an image bitmap.
+	 * Converts a bitmap to bytes.
+	 * Bytes are compressed to 90% quality.
 	 *
-	 * @param imageBitmap image bitmap to convert
-	 * @return String created from the bitmap
+	 * @param imageBitmap bitmap to convert to bytes
+	 * @return byte array of converted bitmap
 	 */
-	public static String bitmapToString(Bitmap imageBitmap) {
-		// Converting the Bitmap image to a byte array
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-		byte[] array = outputStream.toByteArray();
-
-		// Converting the byte array to a string safe for the DB
-		return Base64.encodeToString(array, Base64.URL_SAFE);
-	}
-
 	public static byte[] bitmapToBytes(Bitmap imageBitmap) {
 		// Converting the Bitmap image to a byte array
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -51,16 +31,36 @@ public final class ImageHandler {
 		return outputStream.toByteArray();
 	}
 
+	/**
+	 * Converts a byte array to bitmap object.
+	 *
+	 * @param imageBytes byte array to convert to bitmap
+	 * @return bitmap object from byte array
+	 */
 	public static Bitmap bytesToBitmap(byte[] imageBytes) {
 		return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
 	}
 
+	/**
+	 * Converts a byte array to a BitmapDescriptor (for use in map icons).
+	 *
+	 * @param imageBytes byte array to convert
+	 * @return BitmapDescriptor constructed from bytes
+	 */
 	public static BitmapDescriptor bytesToBitmapDescriptor(byte[] imageBytes) {
 		Bitmap image = bytesToBitmap(imageBytes);
-		image = Bitmap.createScaledBitmap(image,100,100, false);
+		image = Bitmap.createScaledBitmap(image, 100, 100, false);
 		return BitmapDescriptorFactory.fromBitmap(image);
 	}
 
+	/**
+	 * Reads a file from internal storage.
+	 *
+	 * @param path path of file to read (internal storage)
+	 * @return FileReturn object containing size and bytes read
+	 * @throws IOException if file does not exist, or for any other IO reason.
+	 * @see ImageHandler.FileReturn
+	 */
 	public static FileReturn readFromFile(String path) throws IOException {
 		byte[] photo;
 		File photoFile = new File(path);
@@ -72,6 +72,10 @@ public final class ImageHandler {
 		return new FileReturn(photo, bytesRead);
 	}
 
+	/**
+	 * FileReturn object, returned in readFromFile method.
+	 * @see ImageHandler#readFromFile
+	 */
 	public static class FileReturn {
 		private final byte[] photo;
 		private final int bytesRead;
@@ -81,10 +85,18 @@ public final class ImageHandler {
 			this.bytesRead = bytesRead;
 		}
 
+		/**
+		 * Gets the photo bytes.
+		 * @return photo bytes
+		 */
 		public byte[] getPhoto() {
 			return photo;
 		}
 
+		/**
+		 * Gets the bytes read when reading file.
+		 * @return number of bytes
+		 */
 		public int getBytesRead() {
 			return bytesRead;
 		}
